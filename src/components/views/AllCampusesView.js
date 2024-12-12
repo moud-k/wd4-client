722adbb03bb650648c80488fe0297b9a941d0705
Mahmoud Kourani <mahmoudkourani@Mahmouds-MBP.lan>
2024-12-12 13:15:51 -0500

    Updated multiple files to allow users to access a view where they could add new campuses

--- src/components/views/AllCampusesView.js
+++ src/components/views/AllCampusesView.js
@@ -26,11 +26,12 @@ const AllCampusesView = (props) => {
           <h4>campus id: {campus.id}</h4>
           <p>{campus.address}</p>
           <p>{campus.description}</p>
+          <img src={campus.imageurl} alt="Image Not Available"></img>
           <hr/>
         </div>
       ))}
       <br/>
-      <Link to={`/`}>
+      <Link to={`/newcampus`}>
         <button>Add New Campus</button>
       </Link>
       <br/><br/>
--- src/store/actions/actionTypes.js
+++ src/store/actions/actionTypes.js
@@ -6,6 +6,9 @@ export const FETCH_ALL_CAMPUSES = "FETCH_ALL_CAMPUSES";
 //Single campus
 export const FETCH_CAMPUS = "FETCH_CAMPUS";
 
+//Add campus
+export const ADD_Campus = "ADD_CAMPUS";
+
 //All students
 export const FETCH_ALL_STUDENTS = "FETCH_ALL_STUDENTS";
 export const ADD_STUDENT = "ADD_STUDENT";
