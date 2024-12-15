150db1375b4d0f092ef8c80820e1d5b33349d817
Mahmoud Kourani <mahmoudkourani@Mahmouds-MBP.lan>
2024-12-14 20:26:46 -0500

    Updated files to include more attributes when adding a student; fixed bug that occurred when a student didn't have a campus

--- src/components/views/StudentView.js
+++ src/components/views/StudentView.js
@@ -14,7 +14,7 @@ const StudentView = (props) => {
       <h4>{"Email: " + student.email}</h4>
       {student.gpa ? <h4>{"GPA: " + (student.gpa).toFixed(2)}</h4> : <h4>{"GPA: N/A"}</h4>}
       <img src={student.imageurl} alt="Image Not Available"/>
-      {student.campus.id ? <h3>{student.campus.name}</h3> : <h3>{"Campus: N/A"}</h3>}
+      {student.campus ? <h3>{student.campus.name}</h3> : <h3>{"Campus: N/A"}</h3>}
     </div>
   );
 
