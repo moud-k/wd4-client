392f619d3ec9cf1a08f2a9f3263b8bdcc09b6ee4
Mahmoud Kourani <mahmoudkourani@Mahmouds-MBP.lan>
2024-12-14 20:36:14 -0500

    Updated files to allow a student's campus to be accessible from the single student view

--- src/components/views/StudentView.js
+++ src/components/views/StudentView.js
@@ -4,6 +4,9 @@ StudentView.js
 The Views component is responsible for rendering web page with data provided by the corresponding Container component.
 It constructs a React component to display the single student view page.
 ================================================== */
+
+import { Link } from "react-router-dom";
+
 const StudentView = (props) => {
   const { student } = props;
 
@@ -14,7 +17,11 @@ const StudentView = (props) => {
       <h4>{"Email: " + student.email}</h4>
       {student.gpa ? <h4>{"GPA: " + (student.gpa).toFixed(2)}</h4> : <h4>{"GPA: N/A"}</h4>}
       <img src={student.imageurl} alt="Image Not Available"/>
-      {student.campus ? <h3>{student.campus.name}</h3> : <h3>{"Campus: N/A"}</h3>}
+      {student.campus ? 
+        <Link to={`/campus/${student.campus.id}`}>
+              <h3>{student.campus.name}</h3>
+        </Link>
+      : <h3>{"Campus: N/A"}</h3>}
     </div>
   );
 
